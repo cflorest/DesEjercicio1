3745b062d4dc62e079f0737e223b44edb0762291
cetecom <cetecom@LCT50429>
2019-03-27 22:43:11 -0300

    Ejercicio 1

---
+++ menuNaveg.php
@@ -0,0 +1,11 @@
+<div class="">
+    <a id="" class="list-group-item caja" href="#"><i class="caja"></i> Ventas Express</a>
+<a id="ing" class="list-group-item caja" href="#"><i class=""></i> Ingresos</a>
+<a id="inf" class="list-group-item" href="#"><i class=""></i> Informes</a>
+<a id="cons"class="list-group-item" href="#"><i class=""></i> Consultas</a>
+<a id="pro1"class="list-group-item" href="#"><i class=""></i> Productos</a>
+<a id="vta2"class="list-group-item" href="#"><i class=""></i> Ventas</a>
+<a id="pro2"class="list-group-item" href="#"><i class=""></i> Productos</a>
+<a id="vta3"class="list-group-item" href="#"><i class=""></i> Ventas</a>
+<a id="bdga"class="list-group-item" href="#"><i class=""></i> Bodega</a>
+</div>
